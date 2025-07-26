b78d801476f8210708580e5afce53bf0aff741b9
gauravsinghkumar03 <gauravdm03@gmail.com>
2025-07-26 13:33:20 +0530

    Time: 0 ms (100%) Space:24.4 MB (66.19%)

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -11,15 +11,26 @@
  */
 class Solution {
 public:
-    void Inorder(TreeNode* root,vector<int>&v){
-        if(root==NULL) return;
-        Inorder(root->left,v);
-        v.push_back(root->val);
-        Inorder(root->right,v);
+    // void Inorder(TreeNode* root,vector<int>&v){
+    //     if(root==NULL) return;
+    //     Inorder(root->left,v);
+    //     v.push_back(root->val);
+    //     Inorder(root->right,v);
+    // }
+    int Inorder(TreeNode* root,int &i,int k){
+        if(root==NULL) return -1;
+        int left= Inorder(root->left,i,k);
+        if(left!=-1){
+            return left;
+        }
+        i++;
+        if(i==k){
+            return root->val;
+        }
+        return Inorder(root->right,i,k);
     }
     int kthSmallest(TreeNode* root, int k) {
-        vector<int>v;
-        Inorder(root,v);
-        return v[k - 1];
+        int i=0;
+        return Inorder(root,i,k);
     }
 };
